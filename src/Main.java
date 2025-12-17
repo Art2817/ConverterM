43382702f89c11a4dfb4d1ec67068b13ee61c1ce
Artem <artem.cherednychenko2817@gmail.com>
2025-12-17 22:36:11 +0100

    Add kilometers to miles conversion

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-
-    private static final double MILE_TO_KM = 1.60934;
+    static final double MILE_TO_KM = 1.60934;
+    static final double KM_TO_MILE = 0.621371;
 
     public static void main(String[] args) {
         System.out.println("Project: ConverterM");
@@ -11,11 +11,20 @@ public class Main {
 
         System.out.print("Enter miles: ");
         double miles = sc.nextDouble();
-        double km = convertMilesToKilometers(miles);
-        System.out.println(miles + " miles = " + km + " kilometers");
+        double kmConverted = convertMilesToKilometers(miles);
+        System.out.println(miles + " miles = " + kmConverted + " kilometers");
+
+        System.out.print("Enter kilometers: ");
+        double km = sc.nextDouble();
+        double milesConverted = convertKilometersToMiles(km);
+        System.out.println(km + " kilometers = " + milesConverted + " miles");
     }
 
     private static double convertMilesToKilometers(double miles) {
         return miles * MILE_TO_KM;
+    }
+
+    private static double convertKilometersToMiles(double km) {
+        return km * KM_TO_MILE;
     }
 }
